7b8eb1f1caf11ad115e6be6c3ce94d8a00846c52
erfanhamdi <erfan.hamdi@gmail.com>
2023-10-17 20:18:34 -0400

    commented the uniform pp

--- uniform_point_process.py
+++ uniform_point_process.py
@@ -2,13 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    # number of points
     N = 1
+    # Simulation box size
     x_max = 2
     y_max = 1
-
+    # generate random points using uniform distribution
     x = np.random.uniform(0, x_max, N)
     y = np.random.uniform(0, y_max, N)
-
+    # plot the points
     plt.scatter(x, y)
     # plot the rectangle patch
     plt.gca().add_patch(plt.Rectangle((0, 0), x_max, y_max, fill=False))
