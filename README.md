6ba1321c3e8e0218fae749850fb1ab01139be9c8
erfanhamdi <erfan.hamdi@gmail.com>
2023-10-17 20:22:41 -0400

    added the binomial pp to readme and docs

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 This repository contains code for simulating and analyzing point processes. Point processes are stochastic models used to describe the occurrence of events in time or space. They have applications in a variety of fields, including neuroscience, ecology, and finance.
 
 ## Contents
-[uniform_point_process.py](./uniform_point_process.py): Python code for generating a random point using a uniform point process.
+- [uniform_point_process.py](./uniform_point_process.py): Python code for generating a random point using a uniform point process.
+- [binomial_point_process.py](./binomial_point_process.py): In this script we generate a point pattern with fixed number of points with uniform probability
--- binomial_point_process.py
+++ binomial_point_process.py
@@ -2,13 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    # number of points
     N = 10
+    # Simulation box size
     x_max = 2
     y_max = 1
-
+    # generate N random points using uniform distribution
     x = np.random.uniform(0, x_max, N)
     y = np.random.uniform(0, y_max, N)
-
+    # plot the points
     plt.scatter(x, y)
     # plot the rectangle patch
     plt.gca().add_patch(plt.Rectangle((0, 0), x_max, y_max, fill=False))
@@ -16,5 +18,5 @@ if __name__ == "__main__":
     plt.gca().add_patch(plt.Rectangle((0.25, 0.25), 0.5, 0.5, fill=True, color='red', alpha=0.5))
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.title("Binomial((())) point process")
+    plt.title("Binomial point process")
     plt.show()
