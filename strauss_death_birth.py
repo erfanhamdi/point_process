1153718210dbbfe9dc7d8b4fdde499ca9ccad393
erfanhamdi <erfan.hamdi@gmail.com>
2023-10-19 01:09:11 -0400

    every simulation step is gathered in a function simstep

--- strauss_death_birth.py
+++ strauss_death_birth.py
@@ -60,10 +60,62 @@ def pair_potential_fn(num_points_inside_r0, beta):
     """
     return np.exp(-beta * num_points_inside_r0)
 
+def initial_grid(n):
+    """
+    Creates a uniform grid of points.
+
+    Args:
+        n (int): The number of points in each dimension of the grid.
+
+    Returns:
+        tuple: A tuple containing two arrays representing the x and y coordinates of the grid points.
+    """
+    x = np.linspace(0, 1, n)
+    y = np.linspace(0, 1, n)
+    xx, yy = np.meshgrid(x, y)
+    return xx, yy
+
+def simulation_step(xx, yy):
+    # kill a point
+    dead_point_idx = death_fn(xx)
+    # generate a new point
+    new_point = birth_fn()
+    print(f"The coordinate of the dead point is: ({xx[dead_point_idx]}, {yy[dead_point_idx]})")
+    # ax.scatter(xx[dead_point_idx], yy[dead_point_idx], marker='x', c='r', s = marker_size)
+    # replace the dead point with the new point
+    xx[dead_point_idx] = new_point[0]
+    yy[dead_point_idx] = new_point[1]
+    # create a list of the pair of points
+    points = list(zip(xx.flatten(), yy.flatten()))
+    flag = True
+    while flag:
+        # check if the new point satisfies the Strauss process criteria
+        dist_mask = check_criteria(points, new_point, r_s)
+        # reshape dist_mask to the shape of xx
+        dist_mask = dist_mask.reshape(xx.shape)
+        number_of_points_inside_r0 = np.sum(dist_mask)
+        phi = pair_potential_fn(number_of_points_inside_r0-1, beta)
+        print("phi =", phi)
+        # sample u from uniform distribution
+        # u = np.random.uniform(0, 1)
+        # we control how many pairs of points can have overlap
+        u = 0.68
+        print("u =", u)
+        if u < phi:
+            print("Accept")
+            flag = False
+        else:
+            print("Reject")
+            new_point = birth_fn()
+            xx[dead_point_idx] = new_point[0]
+            yy[dead_point_idx] = new_point[1]
+        points = list(zip(xx.flatten(), yy.flatten()))
+    return points
+
 if __name__ == "__main__":
     
     # Number of points in a row or column of a uniform grid
-    n = 10
+    n = 5
     # Interaction radius
     r_s = 0.05
     # Particle radius
@@ -73,69 +125,69 @@ if __name__ == "__main__":
     # Marker size for plotting
     marker_size = 30
     # Number of simulation steps
-    sim_steps = 10
+    sim_steps = 100
 
     # create a uniform grid of points
-    x = np.linspace(0, 1, n)
-    y = np.linspace(0, 1, n)
-    xx, yy = np.meshgrid(x, y)
+    xx, yy = initial_grid(n)
     
     fig, ax = plt.subplots()
     
     flag = True
     for i in range(sim_steps):
+        points = simulation_step(xx, yy)
         # set the limits of the plot
-        ax.set_xlim([-0.2, 1.2])
-        ax.set_ylim([-0.2, 1.2])
-        # kill a point
-        dead_point_idx = death_fn(xx)
-        # generate a new point
-        new_point = birth_fn()
-        print(f"The coordinate of the dead point is: ({xx[dead_point_idx]}, {yy[dead_point_idx]})")
-        ax.scatter(xx[dead_point_idx], yy[dead_point_idx], marker='x', c='r', s = marker_size)
-        # replace the dead point with the new point
-        xx[dead_point_idx] = new_point[0]
-        yy[dead_point_idx] = new_point[1]
-        # create a list of the pair of points
-        points = list(zip(xx.flatten(), yy.flatten()))
-        while flag:
-            # check if the new point satisfies the Strauss process criteria
-            dist_mask = check_criteria(points, new_point, r_s)
-            # reshape dist_mask to the shape of xx
-            dist_mask = dist_mask.reshape(xx.shape)
-            number_of_points_inside_r0 = np.sum(dist_mask)
-            phi = pair_potential_fn(number_of_points_inside_r0-1, beta)
-            print("phi =", phi)
-            # sample u from uniform distribution
-            # u = np.random.uniform(0, 1)
-            # we control how many pairs of points can have overlap
-            u = 0.68
-            print("u =", u)
-            if u < phi:
-                print("Accept")
-                flag = False
-            else:
-                print("Reject")
-                new_point = birth_fn()
-                xx[dead_point_idx] = new_point[0]
-                yy[dead_point_idx] = new_point[1]
-            points = list(zip(xx.flatten(), yy.flatten()))
-        ax.scatter(xx[dist_mask], yy[dist_mask], c='g', s = marker_size)
+        # ax.set_xlim([-0.2, 1.2])
+        # ax.set_ylim([-0.2, 1.2])
+        # # kill a point
+        # dead_point_idx = death_fn(xx)
+        # # generate a new point
+        # new_point = birth_fn()
+        # print(f"The coordinate of the dead point is: ({xx[dead_point_idx]}, {yy[dead_point_idx]})")
+        # ax.scatter(xx[dead_point_idx], yy[dead_point_idx], marker='x', c='r', s = marker_size)
+        # # replace the dead point with the new point
+        # xx[dead_point_idx] = new_point[0]
+        # yy[dead_point_idx] = new_point[1]
+        # # create a list of the pair of points
+        # points = list(zip(xx.flatten(), yy.flatten()))
+        # while flag:
+        #     # check if the new point satisfies the Strauss process criteria
+        #     dist_mask = check_criteria(points, new_point, r_s)
+        #     # reshape dist_mask to the shape of xx
+        #     dist_mask = dist_mask.reshape(xx.shape)
+        #     number_of_points_inside_r0 = np.sum(dist_mask)
+        #     phi = pair_potential_fn(number_of_points_inside_r0-1, beta)
+        #     print("phi =", phi)
+        #     # sample u from uniform distribution
+        #     # u = np.random.uniform(0, 1)
+        #     # we control how many pairs of points can have overlap
+        #     u = 0.68
+        #     print("u =", u)
+        #     if u < phi:
+        #         print("Accept")
+        #         flag = False
+        #     else:
+        #         print("Reject")
+        #         new_point = birth_fn()
+        #         xx[dead_point_idx] = new_point[0]
+        #         yy[dead_point_idx] = new_point[1]
+        #     points = list(zip(xx.flatten(), yy.flatten()))
+        # ax.scatter(xx[dist_mask], yy[dist_mask], c='g', s = marker_size)
         # draw a circle of radius r_s/2 around the new point
         # draw circle of radius r_i around all other points
-        for point in points:
-            circle_i = plt.Circle(point, r_s/2, color='b', fill=False)
-            fig.gca().add_patch(circle_i)
-        circle = plt.Circle(new_point, r_s/2, color='r', fill=False)
-        fig.gca().add_patch(circle)
-        ax.scatter(xx[~dist_mask], yy[~dist_mask], c='b', s = marker_size)
-        ax.set_title(f"Step {i}")
-        # equal aspect ratio
-        fig.gca().set_aspect('equal', adjustable='box')
-        # fig.savefig("figs/strauss_death_birth_step_00" + str(i) + ".jpg")
-        ax.cla()
-        flag = True
-    
+        # for point in points:
+        #     circle_i = plt.Circle(point, r_s/2, color='b', fill=False)
+        #     fig.gca().add_patch(circle_i)
+        # circle = plt.Circle(new_point, r_s/2, color='r', fill=False)
+        # fig.gca().add_patch(circle)
+        # ax.scatter(xx[~dist_mask], yy[~dist_mask], c='b', s = marker_size)
+        # ax.set_title(f"Step {i}")
+        # # equal aspect ratio
+        # fig.gca().set_aspect('equal', adjustable='box')
+        # # fig.savefig("figs/strauss_death_birth_step_00" + str(i) + ".jpg")
+        # ax.cla()
+        # flag = True
+    plt.scatter(*zip(*points))
+    plt.show()
     # plt.figure()
     # # plot the grid
     # import plotly.express as px
