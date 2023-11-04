ec6417896f0d59139472df6395464fb8ea9aa1c1
erfanhamdi <erfan.hamdi@gmail.com>
2023-11-04 13:20:56 -0400

    added more tests

--- plotting.py
+++ plotting.py
@@ -6,7 +6,9 @@ def plot_base(x, y, **kwargs):
     if 'w' in kwargs and 'h' in kwargs:
         w = kwargs['w']
         h = kwargs['h']
-    plt.gca().add_patch(plt.Rectangle((0, 0), w, h, fill=False))
+        plt.gca().add_patch(plt.Rectangle((0, 0), w, h, fill=False))
+    else:
+        plt.gca().add_patch(plt.Rectangle((0, 0), 1, 1, fill=False))
     # if there is a patch argument, plot it
     if 'patch' in kwargs:
         plt.gca().add_patch(plt.Rectangle((0.25, 0.25), 0.5, 0.5, fill=True, color='red', alpha=0.5))
---
+++ test_plotting.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pytest
+from matplotlib.patches import Rectangle
+import matplotlib.pyplot as plt
+from point_process.plotting import plot_base
+
+def test_plot_base():
+    # Test basic functionality with no kwargs
+    x = np.random.rand(10)
+    y = np.random.rand(10)
+    plot_base(x, y)
+    plt.close()
