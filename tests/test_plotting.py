31d597dfddffb5fcd0a516db7a54b0255b13c370
erfanhamdi <erfan.hamdi@gmail.com>
2023-11-04 13:37:43 -0400

    added tests to plotting

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -9,3 +9,20 @@ def test_plot_base():
     y = np.random.rand(10)
     plot_base(x, y)
     plt.close()
+
+def test_plot_base_with_rectangle():
+    # Test plot_base with rectangle patch
+    x = np.random.rand(10)
+    y = np.random.rand(10)
+    w = 0.5
+    h = 0.5
+    plot_base(x, y, w=w, h=h)
+    plt.close()
+
+def test_plot_base_with_patch():
+    # Test plot_base with custom patch
+    x = np.random.rand(10)
+    y = np.random.rand(10)
+    patch = Rectangle((0.25, 0.25), 0.5, 0.5, fill=True, color='red', alpha=0.5)
+    plot_base(x, y, patch=patch)
+    plt.close()
